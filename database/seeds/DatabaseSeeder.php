c7023147b7ddc9aa7ba7e067104f97b144eaa7ee
Mounika Reddy <mounikareddy@northhill.co>
2017-11-16 13:02:23 +0530

    login

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => str_random(50),
+            'email' => str_random(100).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
     }
 }
